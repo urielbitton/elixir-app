f54601ae5c0140d938ba66ed99e7691d2aff4bbd
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2020-09-19 02:16:29 -0400

    Add global subtotal

--- src/comps/CartPageitem.js
+++ src/comps/CartPageitem.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function CartPageItem(props) {
+  const [units, setUnits] = useState(0);
+  let newunits = units;
+  let temp;
 
-  const [units, setUnits] = useState(0)
-  let newunits = units
-  
   function addUnits() {
-    (props.units+newunits)<10?setUnits(prev => prev+1):""
-    if((props.units+newunits)<10)
-      props.updatedadd(props.price)
-  } 
+    props.units + newunits < 10 ? setUnits((prev) => prev + 1) : (temp = 1);
+    if (props.units + newunits < 10) props.updatedadd(props.price);
+  }
   function subUnits() {
-    (props.units+newunits)>1?setUnits(prev => prev-1):""
-    if((props.units+newunits)>1)
-      props.updatedsub(props.price)
-  }  
- 
-  return ( 
+    props.units + newunits > 1 ? setUnits((prev) => prev - 1) : (temp = 1);
+    if (props.units + newunits > 1) props.updatedsub(props.price);
+  }
+
+  return (
     <tr>
-      <td><img src={props.img} alt="cartprod"/><Link to="/product"><h6>{props.name}</h6></Link></td>
+      <td>
+        <img src={props.img} alt="cartprod" />
+        <Link to="/product">
+          <h6>{props.name}</h6>
+        </Link>
+      </td>
       <td className="pricetd">${props.price}.00</td>
-      <td><div className="itemnum"><div className="subnum" onClick={() => subUnits()}>-</div><div className="num">{props.units+newunits}</div><div className="addnum" onClick={() => addUnits()}>+</div></div></td>
-      <td className="itemtotaltd">${props.price*(props.units+newunits)}.00</td>
-      <td><i className="far fa-window-close"></i></td>
+      <td>
+        <div className="itemnum">
+          <div className="subnum" onClick={() => subUnits()}>
+            -
+          </div>
+          <div className="num">{props.units + newunits}</div>
+          <div className="addnum" onClick={() => addUnits()}>
+            +
+          </div>
+        </div>
+      </td>
+      <td className="itemtotaltd">
+        ${props.price * (props.units + newunits)}.00
+      </td>
+      <td>
+        <i className="far fa-window-close"></i>
+      </td>
     </tr>
-  )
-} 
- 
-export default CartPageItem
+  );
+}
+
+export default CartPageItem;
